18058937376d8ce86569692843c1cb2b724adebe
Wela coder <ahmaddogar818@gmail.com>
2025-11-02 23:53:44 +0500

    Add files via upload

---
+++ api/gemini.js
@@ -0,0 +1,28 @@
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+    
+async function runChat(prompt) {
+    const chatSession = model.startChat({
+        generationConfig,
+        history: [
+        ],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    const response = await result.response.text();
+    console.log(response);
+    return response;
+}
+
+export default runChat;
